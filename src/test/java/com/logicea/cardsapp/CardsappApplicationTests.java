a137c1965a0b4e0f4ddc10a48c5fb667a80af0bc
Jason Filippou <jason.filippou@gmail.com>
2023-07-27 18:21:23 +0300

    Basic files and packages.

---
+++ src/test/java/com/logicea/cardsapp/CardsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logicea.cardsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
