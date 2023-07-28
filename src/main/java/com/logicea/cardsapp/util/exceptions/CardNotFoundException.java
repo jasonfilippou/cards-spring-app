3171519e0d813766a34d086c826035af1be29250
Jason Filippou <jason.filippou@gmail.com>
2023-07-28 21:50:11 +0300

    Parameter-less GET ALL working.

--- src/main/java/com/logicea/cardsapp/util/exceptions/CardNotFoundException.java
+++ src/main/java/com/logicea/cardsapp/util/exceptions/CardNotFoundException.java
@@ -3,7 +3,7 @@ package com.logicea.cardsapp.util.exceptions;
 import lombok.Getter;
 
 /**
- * A {@link RuntimeException} thrown when an account with a given ID cannot be found in the DB.
+ * A {@link RuntimeException} thrown when a card with a given ID cannot be found in the DB.
  * @author jason
  */
 @Getter
@@ -12,7 +12,7 @@ public class CardNotFoundException extends RuntimeException {
     private final Long accountId;
 
     public CardNotFoundException(Long accountId) {
-        super("Could not find account with id: " + accountId + ".");
+        super("Could not find card with id: " + accountId + ".");
         this.accountId = accountId;
     }
 }
