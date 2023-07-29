40f089080b581430ed3868db1b313244651289fb
Jason Filippou <jason.filippou@gmail.com>
2023-07-29 21:56:53 +0300

    Made the new fields in the CardDTO Read Only.

--- src/main/java/com/logicea/cardsapp/model/card/CardDto.java
+++ src/main/java/com/logicea/cardsapp/model/card/CardDto.java
@@ -3,6 +3,7 @@ package com.logicea.cardsapp.model.card;
 import static com.logicea.cardsapp.util.Constants.GLOBAL_DATE_TIME_PATTERN;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
@@ -42,18 +43,22 @@ public class CardDto {
     
     @DateTimeFormat(pattern = GLOBAL_DATE_TIME_PATTERN)
     @JsonFormat(pattern = GLOBAL_DATE_TIME_PATTERN)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime createdDateTime;
 
     @Size(min = 5, max = 50)
     @Email
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String createdBy;
 
     @DateTimeFormat(pattern = GLOBAL_DATE_TIME_PATTERN)
     @JsonFormat(pattern = GLOBAL_DATE_TIME_PATTERN)
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private LocalDateTime lastModifiedDateTime;
 
     @Size(min = 5, max = 50)
     @Email
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private String lastModifiedBy;
     
 }
